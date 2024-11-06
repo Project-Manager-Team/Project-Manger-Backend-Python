ef8386e6f51c7f97678e01edb670f1ac40a3088f
Lai Hieu <laihieu2714@gmail.com>
2024-11-06 08:52:13 +0700

    Update ALLOWED_HOSTS to include new IP address

--- backend/settings.py
+++ backend/settings.py
@@ -42,7 +42,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '27.79.222.157', '127.0.0.1', 'www.shadowserver.org']
+ALLOWED_HOSTS = ['localhost', '27.79.131.192', '127.0.0.1', 'www.shadowserver.org']
 
 # Application definition
 
